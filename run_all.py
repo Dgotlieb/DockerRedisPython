6031e9e46b60cb4380b853a85777c6bb577386dd
GiladYered <Gilady83@gmail.com>
2019-01-01 10:48:50 +0200

    add run all file for run selenium test

---
+++ run_all.py
@@ -0,0 +1,23 @@
+import os
+
+from selenium import webdriver
+
+#set driver for test
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'chromedriver')
+driver = webdriver.Chrome(executable_path=filename)
+driver.implicitly_wait(10)
+
+#open browser with url of redis app
+driver.get("http://localhost:5000/")
+driver.maximize_window()
+
+body_text_selector = "//body[contains(text(),'Hello World')]"
+print(driver.find_element_by_xpath(body_text_selector).text)
+
+# notification for test finish succsefuly
+print("redis_app_screen_test finish succsefuly!")
+
+# close the window and the session for next test
+driver.close()
+driver.quit()
